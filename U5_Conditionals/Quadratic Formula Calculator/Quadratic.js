e4074b8e73466061dafa5a70df36c322c63de3b7
UUUSERNAAMEE <115404268+UUUSERNAAMEE@users.noreply.github.com>
2022-12-30 17:56:12 -0600

    Update Quadratic.js

--- U5_Conditionals/Quadratic Formula Calculator/Quadratic.js
+++ U5_Conditionals/Quadratic Formula Calculator/Quadratic.js
@@ -1,13 +1,23 @@
 let elFirstCoefficient = document.getElementById("first-coefficient");
 let elSecondCoefficient = document.getElementById("second-coefficient");
 let elThirdCoefficient = document.getElementById("third-coefficient");
-let elQuadraticSolution = document.getElementById("quadratic-solution");
+
+let elQuadraticSolutionA = document.getElementById("quadratic-solution-a");
+let elQuadraticSolutionB = document.getElementById("quadratic-solution-b");
+
 let CalcQuadratic = document.getElementById("calc-quadratic");
- 
+
 CalcQuadratic.oneclick = function(){
+   
+   let piece = secondCoefficient**2-4*firstCoefficient*thirdCoefficient;
+   if (piece > 0) {
+      let QuadraticSolutionA = (-secondCoefficient + Math.sqrt(piece))/(2*firstCoefficient);
+      let QuadraticSolutionB = (-secondCoefficient - Math.sqrt(piece))/(2*firstCoefficient);
+   
+   
+   
    let firstCoefficient = elfirstCoefficient.value;
    let secondCoefficient = elsecondCoefficient.value;
    let thirdCoefficient = elthirdCoefficient.value;
-   let quadraticSolution = (-secondCoefficient+sqrt(secondCoefficient**2-4(firstCoefficient*thirdCoefficient))/(2*firstCoefficient);
 elQuadraticSolution.value = quadraticSolution;
 }
